77bae48186cd2913277296a8e4e18475e64c4c90
Calvin Xu <calvin.xu.jy@gmail.com>
2021-08-25 17:32:54 +0000

    Modularized input

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 const net = require("net");
 
+
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
@@ -24,24 +25,5 @@ const connect = function () {
 
   return conn;
 };
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function () {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
 
 module.exports = {connect};
