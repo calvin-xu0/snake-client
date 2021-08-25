d6a84d69697a3c4c3d33da7b7125d7c55959a276
Calvin Xu <calvin.xu.jy@gmail.com>
2021-08-25 17:00:18 +0000

    Modularized TCP connection

--- play.js
+++ play.js
@@ -1,26 +1,6 @@
-const net = require("net");
+const {connect} = require("./client")
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "135.23.223.133", // IP address here,
-    // host: "localhost",
-    port: "50542" // PORT number here,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log("Connection established.");
-  });
-  
-  conn.on("data", (data) => {
-    console.log(data);
-  })
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
